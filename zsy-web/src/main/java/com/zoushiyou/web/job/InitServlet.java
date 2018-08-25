e643685848abf4112142a0302bac46c8aad7ccab
邹世友 <zoushy@yonyou.com>
2018-08-25 09:53:48 +0800

    1

--- zsy-web/src/main/java/com/zoushiyou/web/job/InitServlet.java
+++ zsy-web/src/main/java/com/zoushiyou/web/job/InitServlet.java
@@ -1,7 +1,7 @@
 package com.zoushiyou.web.job;
 
-import com.zoushiyou.model.core.UserInfo;
-import com.zoushiyou.service.impl.UserInfoService;
+import com.zoushiyou.model.core.QuartzJob;
+import com.zoushiyou.service.impl.QuartzJobService;
 import com.zoushiyou.web.util.SpringContextUtil;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Component;
@@ -16,9 +16,9 @@ public class InitServlet implements InitializingBean {
 
     @Override
     public void afterPropertiesSet() throws Exception {
-        UserInfoService userInfoService = (UserInfoService) SpringContextUtil.getBean("userInfoService");
-        List<UserInfo> lst = userInfoService.findAll(null);
-        for (UserInfo userInfo : lst) {
+        QuartzJobService userInfoService = (QuartzJobService) SpringContextUtil.getBean("quartzJobService");
+        List<QuartzJob> lst = userInfoService.findAll(null);
+        for (QuartzJob userInfo : lst) {
             System.out.println(userInfo.getId());
         }
     }
